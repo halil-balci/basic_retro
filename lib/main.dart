91b5332ab5c09498ad29aeeafa9d797fe4f73d3e
Halil Balcı <halilbalci878@outlook.com>
2025-07-29 21:42:29 +0300

    deneme

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: const FirebaseOptions(
-      apiKey: "YOUR_API_KEY",
-      authDomain: "YOUR_AUTH_DOMAIN",
-      projectId: "YOUR_PROJECT_ID",
-      storageBucket: "YOUR_STORAGE_BUCKET",
-      messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-      appId: "YOUR_APP_ID",
+      
     ),
   );
   runApp(const MyApp());
